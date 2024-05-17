3da41818300b575c5b6a045319108ffd6c6341cc
Shironeko2406 <hieuntse161485@fpt.edu.vn>
2024-05-17 16:53:06 +0700

    tạo src

---
+++ react/src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import Header from './User/Component/Header.jsx'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <div>
+    <Header></Header>
+  </div>
+)
