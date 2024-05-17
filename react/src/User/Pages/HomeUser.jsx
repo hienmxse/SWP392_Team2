b814b387e1f168697bdde74534eb94b6dab525f6
Shironeko2406 <hieuntse161485@fpt.edu.vn>
2024-05-17 22:09:10 +0700

    Update

---
+++ react/src/User/Pages/HomeUser.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import MyHeader from '../Component/MyHeader'
+
+const HomeUser = () => {
+  return (
+    <MyHeader/>
+  )
+}
+
+export default HomeUser
--- react/src/main.jsx
+++ react/src/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import Header from './User/Component/Header.jsx'
+import HomeUser from './User/Pages/HomeUser.jsx'
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <div>
-    <Header></Header>
+    <HomeUser/>
   </div>
 )
