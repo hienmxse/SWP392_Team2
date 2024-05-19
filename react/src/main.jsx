a957eed4c1a67d45e322ca226bd65be90b64d2e2
Shironeko2406 <hieuntse161485@fpt.edu.vn>
2024-05-19 20:04:38 +0700

    update ui login register và phân page

--- react/src/main.jsx
+++ react/src/main.jsx
@@ -2,11 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import HomeUser from './User/Pages/HomeUser.jsx'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from './User/Pages/Login.jsx';
+import Register from './User/Pages/Register.jsx';
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <div>
-    <HomeUser/>
-  </div>
+  <BrowserRouter>
+      <Routes>
+        <Route path='' element={<Login></Login>}></Route>
+        <Route path='user' element={<HomeUser></HomeUser>}></Route>
+        <Route path='register' element={<Register></Register>}></Route>
+      </Routes>
+  </BrowserRouter>
 )
